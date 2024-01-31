ad5496f7ad242ffb565b3411bd03a7e650cd6389
Mohamed Ahmed Yousef <81932663+mohamedahmed-cloud@users.noreply.github.com>
2024-01-31 16:17:47 +0000

    finish the course

--- src/components/RQSuperHeros.page.tsx
+++ src/components/RQSuperHeros.page.tsx
@@ -1,5 +1,7 @@
+import { useState } from "react";
 import useRQSuperHero from "../hooks/RQSuperHero";
 import { Link } from "react-router-dom";
+import {UseAddRQSuperHero} from "../hooks/RQSuperHero"
 interface SuperHeroDataType {
   isLoading: boolean;
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -15,8 +17,10 @@ interface SuperHeroDataType {
 }
 
 export default function SuperHeroesPage() {
+  const [hero, setHero] = useState({"name":"", "alterEgo":""})
   const { isLoading, data, isError, error }: SuperHeroDataType =
     useRQSuperHero();
+    const {mutate, isLoading: addIsLoading, isErrorLaddIsError, error:addError} = UseAddRQSuperHero();
 
   if (isLoading) {
     return <h2>Loading...</h2>;
@@ -24,9 +28,16 @@ export default function SuperHeroesPage() {
   if (isError ) {
     return <h1>{error.message}</h1>;
   }
+  const handleOnClick = () => {
+    // console.log(hero)
+    mutate(hero)
+  }
 
   return (
     <>
+      <input type="text" name="name" onChange={(e) => setHero({...hero, name: e.target.value})}/>
+      <input type="text" name="alterEgo" onChange={(e) => setHero({...hero, alterEgo: e.target.value})}/>
+      <button onClick={handleOnClick}>Send</button>
       <h2>Super Heroes Page</h2>
       {JSON.stringify(data)}
       {/* <button onClick={refetch}>Refetch Data</button> */}
--- src/hooks/RQSuperHero.tsx
+++ src/hooks/RQSuperHero.tsx
@@ -1,16 +1,56 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import axios from "axios";
+// import { request } from "../utils/axios-utils";
+async function fetchRQSuperHero () {
+  const response = await axios.get("http://localhost:4000/superheroes");
+  // const response = await request({url:"/superheroes"})
+  console.log(response)
+  return response.data
+}
+async function addRQSuperHero(hero: {name: string, alterEgo: string}) {
+  const response = await axios.post("http://localhost:4000/superheroes", hero);
+  // const response = await request({url:"/superheroes", method:"post", data:hero})
+  return response.data
+}
 
 
-
-export default function useRQSuperHeroSpecific(){
+export default function useFetchRQSuperHeros(){
     return useQuery({
         queryKey:["superheroes"],
-        queryFn: async () => {
-          const response = await axios.get("http://localhost:4000/superheroes");
-          return response.data
-        },
+        queryFn: fetchRQSuperHero,
         select: (data) => {
             return data
         } 
 })}
+
+export  function UseAddRQSuperHero() {
+  const queryClient = useQueryClient()
+  return useMutation({
+    mutationFn : (hero: {name: string, alterEgo: string}) => addRQSuperHero(hero),
+    // onSuccess: (data) => {
+    //   queryClient.setQueryData(['superheroes'], (oldQueryData: {name: string, alterEgo: string}[]) => {
+    //     return [...oldQueryData, data]
+    //   })
+    //   // queryClient.invalidateQueries({queryKey: ["superheroes"]})
+    // }
+    //can any outgoing refreshes that can be overwrite our optimistic updates.
+    onMutate:(hero) => {
+      queryClient.cancelQueries({queryKey: ["superheroes"]})
+      const previousHeroList = queryClient.getQueryData(['superheroes'])
+      queryClient.setQueryData(['superheroes'], (oldQueryData: {name: string, alterEgo: string}[]) => {
+        return [...oldQueryData, hero]
+      })
+
+      // call if the queryClient is failed.
+      return previousHeroList
+    },
+    onError: (_error, _hero, context) => {
+      // context contain additional information related to the mutation.
+      return queryClient.setQueryData(['superheroes'], context.previousHeroList)
+    },
+    // called when success and error
+    onSettled: () => {
+      queryClient.invalidateQueries({queryKey: ["superheroes"]})
+    },
+  })
+}
